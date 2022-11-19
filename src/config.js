610a74c4eab55a2f0a32a8b927ce99c7b04aa2d8
Benjamín Larenas <alonsobenjaminlb@gmail.com>
2022-11-19 13:09:53 -0400

    cambiovalor OR DB_PORT 5432 6953

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'postgres'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'root'
 const DB_NAME = process.env.DB_NAME || 'postgres'
-const DB_PORT = process.env.DB_PORT || 5432
+const DB_PORT = process.env.DB_PORT || 6953
 
 
 module.exports = { DB_PORT,DB_HOST,DB_NAME,DB_PASSWORD,DB_USER,PORT}
